8c5ba33ed25b6d7203c711eb12a745a3c71ed0db
Jazz Kersell <jazzkersell@gmail.com>
2015-02-01 15:12:59 -0500

    Lay out states.

--- assgn1/commentStripper.pas
+++ assgn1/commentStripper.pas
@@ -1,17 +1,34 @@
 program ReadFile;
-
+type
+    states = (start, slash, quote, slashSlash, slashStar, slashStarStar);
 var
     myFile: text;
     filename: string;
     c: char;
+    state: states;
 begin
+    state := start;
     filename := 'testFile';
     assign(myFile, filename);
     reset(myFile);
     while not eof(myFile) do
     begin
         read(myFile, c);
-        write(c);
+        case state of
+            start:
+                {Just write directly to output}
+                write(c);
+            slash:
+                ;
+            quote:
+                ;
+            slashSlash:
+                ;
+            slashStar:
+                ;
+            slashStarStar:
+                ;
+        end;
     end;
     close(myFile);
 end.
