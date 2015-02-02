415b82d88ce993ea61d3d1c093df3e34ba08074d
Jazz Kersell <jazzkersell@gmail.com>
2015-02-02 13:48:53 -0500

    Move transition function into seperate function.

--- assgn1/commentStripper.pas
+++ assgn1/commentStripper.pas
@@ -6,9 +6,75 @@ var
     outputFile: text;
     filename: string;
     c: char;
-    state: states;
+    currentState: states;
+function transitionFunction(state: states; c: char): states;
 begin
-    state := start;
+    transitionFunction := state;
+    case state of
+        start:
+            begin
+            writeln('State is start');
+            if c = '/' then
+                transitionFunction := slash
+            else if c = '"' then
+                transitionFunction := quote
+            else
+                ;
+            end;
+        slash:
+            begin
+            writeln('State is slash');
+            if c = '/' then
+                transitionFunction := slashSlash
+            else if c = '*' then
+                transitionFunction := slashStar
+            else
+                transitionFunction := start;
+            end;
+        slashSlash:
+            begin
+            writeln('State is slashSlash');
+            if EOLn(inputFile) then
+                transitionFunction := start
+            else
+                ;{dump all characters until end of line}
+            end;
+        slashStar:
+            begin
+            writeln('State is slashStar');
+            if c = '*' then
+                transitionFunction := slashStarStar
+            else
+                ;{dump all characters until comment is ended}
+            end;
+        slashStarStar:
+            begin
+            writeln('State is slashStarStar');
+            if c = '/' then
+                transitionFunction := start
+            else
+                transitionFunction := slashStar;
+            end;
+        quote:
+            begin
+            writeln('State is quote');
+            if c = '\' then
+                transitionFunction := quoteBackslash
+            else if c = '"' then
+                transitionFunction := start
+            else
+                ;
+            end;
+        quoteBackslash:
+            begin
+            writeln('State is quoteBackslash');
+            {TODO: handle escaped characters properly}
+            transitionFunction := quote;
+            end;
+    end;
+end;
+begin
+    currentState := start;
     filename := 'testFile';
     assign(inputFile, filename);
     reset(inputFile);
@@ -17,68 +83,7 @@ begin
     while not eof(inputFile) do
     begin
         read(inputFile, c);
-        case state of
-            start:
-                begin
-                writeln('State is start');
-                if c = '/' then
-                    state := slash
-                else if c = '"' then
-                    state := quote
-                else
-                    ;
-                end;
-            slash:
-                begin
-                writeln('State is slash');
-                if c = '/' then
-                    state := slashSlash
-                else if c = '*' then
-                    state := slashStar
-                else
-                    state := start;
-                end;
-            slashSlash:
-                begin
-                writeln('State is slashSlash');
-                if EOLn(inputFile) then
-                    state := start
-                else
-                    ;{dump all characters until end of line}
-                end;
-            slashStar:
-                begin
-                writeln('State is slashStar');
-                if c = '*' then
-                    state := slashStarStar
-                else
-                    ;{dump all characters until comment is ended}
-                end;
-            slashStarStar:
-                begin
-                writeln('State is slashStarStar');
-                if c = '/' then
-                    state := start
-                else
-                    state := slashStar;
-                end;
-            quote:
-                begin
-                writeln('State is quote');
-                if c = '\' then
-                    state := quoteBackslash
-                else if c = '"' then
-                    state := start
-                else
-                    ;
-                end;
-            quoteBackslash:
-                begin
-                writeln('State is quoteBackslash');
-                {TODO: handle escaped characters properly}
-                state := quote;
-                end;
-        end;
+        currentState := transitionFunction(currentState, c);
     end;
     close(inputFile);
     close(outputFile);
