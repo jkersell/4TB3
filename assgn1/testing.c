9d1429fd1b4c9c90cead0b73ee064e02f807b41b
Jazz Kersell <jazzkersell@gmail.com>
2015-02-04 13:27:36 -0500

    More rigorous testing.

--- assgn1/testing.c
+++ assgn1/testing.c
@@ -2,6 +2,9 @@
 
 /* This is a "multiline" comment /*and //should be removed.*/
 int main(int argc, char *argv[]) {
+    /* Here is */ int i = 0; /* some *really
+                                weird commenting*/
+    /* and even */ int j = 3; // weird commenting
     // This comment should go away "as well"
     printf("Hello /*comment*/ //\"stripping\"!\n");
 }
