868100d203f6dc0c2f480531f52b38210d524aa1
Jazz Kersell <jazzkersell@gmail.com>
2015-02-04 16:07:26 -0500

    factor out input validation

--- assgn1/subset.pas
+++ assgn1/subset.pas
@@ -21,6 +21,31 @@ var
     line, nextWord: string;
     i, lineNumber, numWords, numTransitions: integer;
 
+function validateLine(lineNumber: integer; line: string): integer;
+var
+    numWords: integer;
+begin
+    numWords := wordCount(line, wordDelim);
+
+    if lineNumber = 0 then
+    begin
+        if numWords <> 1 then
+        begin
+            writeln('There should only be one state on the first line.');
+            exit;
+        end;
+    end
+    else if lineNumber > 1 then
+        if numWords <> 3 then
+        begin
+            writeln('Lines 3 and up must have format: <state> <symbol> <state>');
+            exit;
+        end;
+
+    {return number of words in this line}
+    validateLine := numWords;
+end;
+
 begin
     wordDelim := [' '];
     lineNumber := 0;
@@ -28,23 +53,8 @@ begin
     while not eof do
     begin
         readln(line);
-        numWords := wordCount(line, wordDelim);
 
-        {validate file format}
-        if lineNumber = 0 then
-        begin
-            if numWords <> 1 then
-            begin
-                writeln('There should only be one state on the first line.');
-                exit;
-            end;
-        end
-        else if lineNumber > 1 then
-            if numWords <> 3 then
-            begin
-                writeln('Lines 3 and up must have format: <state> <symbol> <state>');
-                exit;
-            end;
+        numWords := validateLine(lineNumber, line);
 
         for i := 1 to numWords do
         begin
