0b7dfa4205c48b5cda8686856bde7458297c4f40
Jazz Kersell <jazzkersell@gmail.com>
2015-03-25 17:28:55 -0400

    Add Tree constructor.

--- assgn3/part2/parser.pas
+++ assgn3/part2/parser.pas
@@ -8,12 +8,20 @@ interface
         child : ^Node;
         next : ^Node;
         content : Contents; end;
+    type Tree = record
+        root : ^Node;
+        currentTable : ^Node;
+        currentRow : ^Node;
+        currentCell : ^Node; end;
+
+    var parseTree : Tree;
 
     procedure Parse;
 
 implementation
     type State = (Outside, ParseTable, ParseRow, ParseCell);
     type nodePtr = ^Node;
+    type treePtr = ^Tree;
 
     var parseState : State;
     var error : boolean;
@@ -26,6 +34,17 @@ implementation
         newNode^.child := nil;
         newNode^.next := nil;
         ConstructNode := newNode;
+    end;
+
+    function ConstructParseTree : treePtr;
+    var
+        newTree : treePtr;
+    begin
+        newTree^.root := nil;
+        newTree^.currentTable := nil;
+        newTree^.currentRow := nil;
+        newTree^.currentCell := nil;
+        ConstructParseTree := newTree;
     end;
 
     procedure parseError(msg : string);
