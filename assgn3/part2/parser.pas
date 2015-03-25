98f2b988c680b48d67e684249e73f86bc24cab56
Jazz Kersell <jazzkersell@gmail.com>
2015-03-25 16:50:26 -0400

    Implement state changing.

--- assgn3/part2/parser.pas
+++ assgn3/part2/parser.pas
@@ -33,19 +33,59 @@ implementation
         begin
             case sym of
             TableStartSym:
+            begin
                 writeln('TableStartSym');
+                if (parseState = Outside) then
+                    parseState := ParseTable
+                else
+                    parseError('Unexpected tag: <TABLE>');
+            end;
             TableEndSym:
+            begin
                 writeln('TableEndSym');
+                if (parseState = ParseTable) then
+                    parseState := Outside
+                else
+                    parseError('Unexpected tag: </TABLE>');
+            end;
             RowStartSym:
+            begin
                 writeln('RowStartSym');
+                if (parseState = ParseTable) then
+                    parseState := ParseRow
+                else
+                    parseError('Unexpected tag: <TR>');
+            end;
             RowEndSym:
+            begin
                 writeln('RowEndSym');
+                if (parseState = ParseRow) then
+                    parseState := ParseTable
+                else
+                    parseError('Unexpected tag: </TR>');
+            end;
             CellStartSym:
+            begin
                 writeln('CellStartSym');
+                if (parseState = ParseRow) then
+                    parseState := ParseCell
+                else
+                    parseError('Unexpected tag: <TD>');
+            end;
             CellEndSym:
+            begin
                 writeln('CellEndSym');
+                if (parseState = ParseCell) then
+                    parseState := ParseRow
+                else
+                    parseError('Unexpected tag: </TD>');
+            end;
             ContentsSym:
+            begin
                 writeln('ContentsSym');
+                if (parseState = ParseCell) then
+                    writeln('Cell contents');
+            end;
             end;
             GetSym;
         end;
